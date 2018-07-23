3cb1bbb5c9260628af7b3626e8dad6b3448c31a5
龚佐龙 <gzl@shtruecolor.com>
2018-07-24 01:35:12 +0800

    修改

--- application/library/TCApiControllerBase.php
+++ application/library/TCApiControllerBase.php
@@ -12,6 +12,7 @@ class TCApiControllerBase extends TCControllerBase {
     $this->getView()->layout = null;
     header('Access-Control-Allow-Origin:' . "*");
     header('Access-Control-Allow-Methods:' . "*");
+    header('Access-Control-Request-Headers:' . "*");
   }
 }
 
