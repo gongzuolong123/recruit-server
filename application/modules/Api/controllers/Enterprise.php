6fbd778be71cbd1faee39846911a51b07cf9a3da
龚佐龙 <gzl@shtruecolor.com>
2018-07-25 01:51:35 +0800

    修改

--- application/modules/Api/controllers/Enterprise.php
+++ application/modules/Api/controllers/Enterprise.php
@@ -19,12 +19,13 @@ class EnterpriseController extends TCApiControllerBase {
    */
   public function recruitListAction() {
     $page = intval($_GET['page']);
-    $limit = 15;
+    $limit = 8;
     $offset = $page * $limit;
     $data = [];
     $models = RecruitModel::findAllByAttributes(['status' => [0, -1]], 'weight', "{$offset},{$limit}");
     foreach($models as $model) {
       $item = new stdClass();
+      $item->id = $model->id;
       $item->enterpriseName = $model->getEnterpriseModel()->name;
       $item->industryName = IndustryModel::findById($model->getEnterpriseModel()->industry_id)->name;
       $item->areaName = AreaModel::findById($model->getEnterpriseModel()->area_id)->name;
