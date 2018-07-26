a4e8c933171105a153867af7f6fb3088c0158f6b
龚佐龙 <gzl@shtruecolor.com>
2018-07-26 23:50:17 +0800

    接口修改

--- application/modules/Api/controllers/Image.php
+++ application/modules/Api/controllers/Image.php
@@ -9,7 +9,18 @@ class ImageController extends TCApiControllerBase {
    * 上传
    */
   public function uploadAction() {
-    return $this->writeSuccessJsonResponse($_FILES);
+    if($_FILES['license'] && $_FILES['license']['error'] == 0) {
+      $file_name = md5(uniqid()) . '.jpg';
+      $file_path = '/image/license/';
+      if(!is_dir(APPLICATION_PATH . $file_path)) mkdir(APPLICATION_PATH . $file_path,0777, true);
+      $status = move_uploaded_file($_FILES['license']['tmp_name'],APPLICATION_PATH . $file_path . $file_name);
+      if($status) {
+        $data = new stdClass();
+        $data->url = Yaf_Application::app()->getConfig()->get('api.root.url') . $file_path . $file_name;
+        return $this->writeSuccessJsonResponse($data);
+      }
+    }
+    return $this->writeErrorJsonResponseCaseParamsError();
   }
 
 
