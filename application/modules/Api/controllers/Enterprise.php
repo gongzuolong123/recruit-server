4da4e7d6ba216eb083fcfce04057a48a8f0fe11d
龚佐龙 <gzl@shtruecolor.com>
2018-07-27 00:00:53 +0800

    修改

--- application/modules/Api/controllers/Enterprise.php
+++ application/modules/Api/controllers/Enterprise.php
@@ -47,7 +47,8 @@ class EnterpriseController extends TCApiControllerBase {
     $data->areaId = $model->area_id;
     $data->areaName = AreaModel::findById($model->area_id)->name;
     $data->shopName = $model->shop_name;
-    $data->licese = Yaf_Application::app()->getConfig()->get('api.root.url') . $model->license;
+    $data->licese = $model->license;
+    $data->licese_url = Yaf_Application::app()->getConfig()->get('api.root.url') . $model->license;
     $data->address = $model->address;
     return $this->writeSuccessJsonResponse($data);
   }
