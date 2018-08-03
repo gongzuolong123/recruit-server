dadca6ff6de2c21f649c90003ce1b90b58c8280e
龚佐龙 <gzl@shtruecolor.com>
2018-08-03 23:56:55 +0800

    接口修改

--- application/modules/Api/controllers/Upload.php
+++ application/modules/Api/controllers/Upload.php
@@ -25,4 +25,25 @@ class UploadController extends TCApiControllerBase {
   }
 
 
+  /**
+   * 上传csv文件
+   */
+  public function csvAction() {
+    if($_FILES['csv'] && $_FILES['csv']['error'] == 0) {
+      return $this->writeSuccessJsonResponse($_FILES);
+      $file_name = md5(uniqid()) . '.jpg';
+      $file_path = '/image/license/';
+      if(!is_dir(APPLICATION_PATH . $file_path)) mkdir(APPLICATION_PATH . $file_path,0777, true);
+      $status = move_uploaded_file($_FILES['license']['tmp_name'],APPLICATION_PATH . $file_path . $file_name);
+      if($status) {
+        $data = new stdClass();
+        $data->url = Yaf_Application::app()->getConfig()->get('api.root.url') . $file_path . $file_name;
+        $data->image_path = $file_path . $file_name;
+        return $this->writeSuccessJsonResponse($data);
+      }
+    }
+    return $this->writeErrorJsonResponseCaseParamsError();
+  }
+
+
 }
