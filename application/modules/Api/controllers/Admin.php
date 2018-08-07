6e628f2680bcbf82f1d650907e29e73ad8d52650
龚佐龙 <gzl@shtruecolor.com>
2018-08-08 00:53:29 +0800

    登陆修改

--- application/modules/Api/controllers/Admin.php
+++ application/modules/Api/controllers/Admin.php
@@ -8,16 +8,22 @@
 
 class AdminController extends TCApiControllerBase {
 
+  private $users = [
+    'admin' => 'admin',
+  ];
+
   /**
    * 管理员登陆
    */
   public function loginAction() {
     $userName = $_POST['user_name'];
     $passWord = $_POST['pass_word'];
-    if($userName == 'admin' && $passWord == 'admin') {
-      $_SESSION['user'] = 'admin';
-      return $this->writeSuccessJsonResponse();
+    if(isset($this->users[$userName]) && $this->users[$userName] == $passWord) {
+      $token = md5(uniqid());
+      TCRedisManager::getInstance()->redis->set($token,'admin',86400 * 7);
+      return $this->writeSuccessJsonResponse(['user' => 'admin', 'token' => $token]);
     }
+
     return $this->writeErrorJsonResponseCaseParamsError();
   }
 
