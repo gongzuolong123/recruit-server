9ce8e6848e81ae2554191a9c1f682afcb8a16ccb
龚佐龙 <gzl@shtruecolor.com>
2018-08-11 10:34:00 +0800

    修改

--- application/library/TCApiControllerBase.php
+++ application/library/TCApiControllerBase.php
@@ -11,8 +11,6 @@ class TCApiControllerBase extends TCControllerBase {
     Yaf_Dispatcher::getInstance()->autoRender(false);
     $this->getView()->layout = null;
     header('Access-Control-Allow-Origin:' . "*");
-    header('Access-Control-Allow-Methods:' . "*");
-    header('Access-Control-Request-Headers:' . " Origin, X-Requested-With, Content-Type, Accept,content-type");
   }
 
 
