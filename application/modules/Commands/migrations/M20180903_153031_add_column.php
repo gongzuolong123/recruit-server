b7a61e5bd9d00bef0f00a9d8d6c45bd07b9ca057
龚佐龙 <gzl@shtruecolor.com>
2018-09-03 21:37:57 +0800

    bug fix

--- application/modules/Commands/migrations/M20180903_153031_add_column.php
+++ application/modules/Commands/migrations/M20180903_153031_add_column.php
@@ -2,8 +2,8 @@
 
 class M20180903_153031_add_column extends TCMigrationBase {
   public function up() {
-    $this->addColumn('recruits', 'refresh_time', 'datetime');
-    $this->createIndex('refresh_time','recruits','datetime');
+    //$this->addColumn('recruits', 'refresh_time', 'datetime');
+    $this->createIndex('refresh_time','recruits','refresh_time');
   }
 
   public function down() {
