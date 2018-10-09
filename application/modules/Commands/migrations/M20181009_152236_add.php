25ea8434acceca6ac644abae89cff3a82705f982
龚佐龙 <gzl@shtruecolor.com>
2018-10-09 22:00:28 +0800

    添加表

---
+++ application/modules/Commands/migrations/M20181009_152236_add.php
@@ -0,0 +1,28 @@
+<?php
+
+class M20181009_152236_add extends TCMigrationBase {
+  public function up() {
+    $this->createTable('history_call_phone', [
+      'id' => 'integer not null primary key auto_increment',
+      'user_id' => 'int not null default 0',
+      'enterprise_id' => 'int not null default 0',
+      'updated_at' => 'datetime',
+      'key user_id (user_id)',
+      'key updated_at (updated_at)'
+    ]);
+    $this->addColumn('users','type','tinyint not null default 0');
+    $this->createTable('user_profiles', [
+      'id' => 'integer not null primary key',
+      'name' => 'varchar(255) not null default ""',
+      'gender' => 'tinyint not null default 0',
+      'birth_date' => 'date',
+      'city' => 'varchar(255) not null default ""',
+    ]);
+  }
+  
+  public function down() {
+    $this->dropTable('history_call_phone');
+    $this->dropColumn('users','type');
+    $this->dropTable('user_profiles');
+  }
+}
