24268614d2180e9127613cce631e05a70ddd7ea6
龚佐龙 <gzl@shtruecolor.com>
2018-10-09 11:32:31 +0800

    添加接口

---
+++ application/views/documents/documentfortrack.php
@@ -0,0 +1,19 @@
+<pre>
+  <b>请严格按照上面格式下列格式上传数据</b>
+  数据上传格式：
+  {
+    "page": "special",           // 哪个页面
+    "extra_data": {              // 页面自定义事件
+    },
+    "device": {                                   // 设备基本信息
+    },
+  }
+
+  特殊自定义事件跟踪，该部分数据请以json格式放入参数extra_data中
+
+  1.其它 page: other
+  {
+    "enterpriseId": "1"      // 企业id
+    "type": "call_phone"     // call_phone: 拔号
+  }
+</pre>
