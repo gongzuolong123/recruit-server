cf190719baac5fb683f9def8385daa8afee7a3f6
龚佐龙 <gzl@shtruecolor.com>
2018-10-14 15:19:33 +0800

    修改

--- application/modules/Api/controllers/History.php
+++ application/modules/Api/controllers/History.php
@@ -12,7 +12,7 @@ class HistoryController extends TCApiControllerBase {
 
   /**
    * 上传打电话记录
-   * @param $enterprise_id  企业id
+   * @param $enterprise_id  招聘id
    */
   public function callPhoneUpAction() {
     if(!$this->current_user || empty($_POST['enterprise_id'])) return $this->writeErrorJsonResponseCaseParamsError();
@@ -40,10 +40,33 @@ class HistoryController extends TCApiControllerBase {
     $sql = "select * from history_call_phone where user_id={$this->current_user->id} order by updated_at desc";
     $rows = TCDbManager::getInstance()->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     foreach($rows as $row) {
-      $enterpriseModel = EnterpriseModel::findById($row['enterprise_id']);
+      $recruitModel = RecruitModel::findById($row['enterprise_id']);
       $item = new stdClass();
-      $item->id = $enterpriseModel->id;
-      $item->name = $enterpriseModel->name;
+      $item->id = $recruitModel->id;
+      $item->enterpriseName = $recruitModel->getEnterpriseModel()->name;
+      $item->shopName = $recruitModel->getEnterpriseModel()->shop_name;
+      $item->industryName = IndustryModel::findById($recruitModel->getEnterpriseModel()->industry_id)->name;
+      $item->areaName = AreaModel::findById($recruitModel->getEnterpriseModel()->area_id)->name;
+      $item->workAddress = $recruitModel->work_address;
+      $item->workPost = $recruitModel->work_post;
+      $item->workRequire = $recruitModel->work_require;
+      $item->status = $recruitModel->status;
+      $item->weight = $recruitModel->weight;
+      $item->contactsName = $recruitModel->contacts_name;
+      $item->contactsPhone = $recruitModel->contacts_phone;
+      $item->areaNameAll = AreaModel::getAllAreaName($recruitModel->getEnterpriseModel()->area_id, 2);
+      $item->wagesType = $recruitModel->wages_type;
+      $item->wages1 = $recruitModel->wages_1;
+      $item->wages2 = $recruitModel->wages_2;
+      $wages = $item->wages1 . '-' . $item->wages2;
+      if($item->wages2 <= 0) $wages = $recruitModel->wages_1 . '以上';
+      if($item->wages1 <= 0 && $item->wages2 <= 0) $wages = '面议';
+      $item->wages = $wages;
+      $item->education = $recruitModel->education;
+      $item->updated_at = $recruitModel->updated_at;
+      $item->tagNames = RecruitTagModel::getTagNamesByRecruitId($model->id);
+      $item->recommend = false;
+      if($model->status == 1) $item->recommend = true;
       $data[] = $item;
     }
 
